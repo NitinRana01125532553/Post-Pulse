5a959e44fc8390bb678078670953aa6ae44fab70
Nitin <nitinrana01125532553@gmail.com>
2023-09-14 11:17:35 +0530

    key-bug

--- src/app/components/Store.tsx
+++ src/app/components/Store.tsx
@@ -4,10 +4,11 @@ import Card from './Card';
 function Store({shirts, shoes, pants}) {
   return (
     <div className="min-h-screen px-14 py-6">
-      <h1 className='text-2xl font-bold mb-6'>Shirts</h1>
+      <h1 className="text-2xl font-bold mb-6">Shirts</h1>
       <div className="flex flex-row gap-6 overflow-x-scroll mb-14">
-        {shirts.map((item) => (
+        {shirts.map((item, idx) => (
           <Card
+            key={idx}
             title={item.title}
             desc={item.desc}
             image={item.image}
@@ -15,10 +16,11 @@ function Store({shirts, shoes, pants}) {
           />
         ))}
       </div>
-      <h1 className='text-2xl font-bold mb-6'>Shoes</h1>
+      <h1 className="text-2xl font-bold mb-6">Shoes</h1>
       <div className="flex flex-row gap-8 overflow-x-scroll mb-14">
-        {shoes.map((item) => (
+        {shoes.map((item, idx) => (
           <Card
+            key={idx}
             title={item.title}
             desc={item.desc}
             image={item.image}
@@ -26,10 +28,11 @@ function Store({shirts, shoes, pants}) {
           />
         ))}
       </div>
-      <h1 className='text-2xl font-bold mb-6'>Pants</h1>
+      <h1 className="text-2xl font-bold mb-6">Pants</h1>
       <div className="flex flex-row gap-8 overflow-x-scroll">
-        {pants.map((item) => (
+        {pants.map((item, idx) => (
           <Card
+            key={idx}
             title={item.title}
             desc={item.desc}
             image={item.image}
